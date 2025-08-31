1ca90c6007509e27783364dad9722ec38d1dad3c
esraaKamel <esraaKamel1811@gmail.com>
2025-09-01 01:05:28 +0200

    enhance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,27 +13,34 @@ import {
   OnDestroy,
   ChangeDetectorRef,
   ChangeDetectionStrategy,
+  WritableSignal,
+  signal,
+  computed,
+  effect,
+  EffectRef,
 } from '@angular/core';
 import { COURSES } from './Data/db-data';
 import { ICourse } from './Interfaces/course';
 import { CourseCardComponent } from './course-card/course-card.component';
 import { AccountService } from './services/account.service';
 import { EmployeeService } from './services/employee.service';
+import { CounterService } from './services/counter.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent
   implements
-  OnInit,
-  OnChanges,
-  AfterViewInit,
-  DoCheck,
-  AfterViewChecked,
-  OnDestroy {
+    OnInit,
+    OnChanges,
+    AfterViewInit,
+    DoCheck,
+    AfterViewChecked,
+    OnDestroy
+{
   title = 'Full Demo';
   courses: Array<ICourse> = COURSES;
   @ViewChild('container') container: ElementRef;
@@ -50,18 +57,78 @@ export class AppComponent
   accounts = [];
   employeeActivated: boolean = false;
 
+  counter: WritableSignal<number> = signal(2);
+  derviedCounter = computed(() => {
+    const counter = this.counter();
+    return counter * 10;
+  });
+
+  course: WritableSignal<{ id: number; title: string }> = signal({
+    id: 1,
+    title: 'testtttttttttttttttt',
+  });
+  newCourses: WritableSignal<string[]> = signal([
+    'New course 1',
+    'New course 2',
+    'New course 3',
+  ]);
+
+  multiplier: number = 0;
+  effectRef: EffectRef;
   constructor(
+    private counterService: CounterService,
     private accountService: AccountService,
     private employeeService: EmployeeService,
     private cd: ChangeDetectorRef
-  ) { }
+  ) {
+    const readOnlySignal = this.counter.asReadonly();
+
+    this.effectRef = effect(() => {
+      const counterValue = this.counterService.counter();
+      const derviedCounter = this.derviedCounter();
+      // log data
+      console.log(
+        `Counter value ${counterValue} and dervied counter is ${derviedCounter}`
+      );
+      localStorage.setItem('Counter value', counterValue.toString());
+    }, {
+      manualCleanup: true
+    });
+  }
 
   ngOnInit(): void {
-    // console.log('from ngOnInit function');
+    console.log('from ngOnInit function');
+    console.log('this.derviedCounter', this.derviedCounter());
+
     this.accounts = this.accountService.accounts;
     this.employeeService.activatedEmitter.subscribe((data) => {
       this.employeeActivated = data;
-    })
+    });
+
+    this.increment();
+  }
+
+  increment() {
+    // this.counter++;
+    // this.counter.set(this.counter() + 1);
+    // this.counter.update((value) => value + 1);
+
+    this.counterService.increment();
+    this.course.set({ id: 2, title: 'new course after update' });
+    this.newCourses.set(['new course after update Array']);
+
+    this.newCourses.update((courses) => [
+      ...courses,
+      'new course after update Array',
+    ]);
+  }
+
+  multiplierIncrement() {
+    this.multiplier++;
+  }
+
+  cleanUp() {
+    this.effectRef.destroy();
   }
 
   // onAccountAdded(newAccount: { name: string, status: string }) {
@@ -132,5 +199,4 @@ export class AppComponent
   ngOnDestroy(): void {
     // throw new Error('Method not implemented.');
   }
-
 }
--- src/app/interceptors/auth.interceptor.service.ts
+++ src/app/interceptors/auth.interceptor.service.ts
@@ -1,20 +1,28 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { PostsService } from "../posts/posts.service";
-import { Injectable } from "@angular/core";
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PostsService } from '../posts/posts.service';
+import { Injectable } from '@angular/core';
 
 @Injectable()
-
 export class AuthInterceptorService implements HttpInterceptor {
-
   constructor(private postsService: PostsService) {}
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     // req is immuatble
-    console.log("Request is sending.................");
+    console.log('Request is sending.................');
 
-    if(this.postsService.isLoggedIn) {
-      const modifiedRequest = req.clone({headers: req.headers.append("token","Bearer ffbrnjjjedgrfgrffefe")});
+    if (this.postsService.isLoggedIn) {
+      const modifiedRequest = req.clone({
+        headers: req.headers.append('token', 'Bearer ffbrnjjjedgrfgrffefe'),
+      });
       return next.handle(modifiedRequest);
     } else {
       return next.handle(req);
